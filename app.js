5663ec93d3717b69e02d939c7937ba7bc4b4503b
wogara <weogara@gmail.com>
2024-04-29 11:30:09 -0400

    linting on neovim

--- app.js
+++ app.js
@@ -1,58 +1,52 @@
-const express = require('express');
-const {Server} = require("socket.io");
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const { Server } = require("socket.io");
+const path = require("path");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server,{
-  cors:{
-    origin:'http://localhost:5173',
-    methods: ['GET','POST']
-  }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
-function generateRoomID(){
+function generateRoomID() {
   return uuidv4();
 }
-io.on('connection', (socket) => {
-  
-  socket.on('createRoom', (roomID) => {
-    
-    console.log('create Room ' + roomID)
+io.on("connection", (socket) => {
+  socket.on("createRoom", (roomID) => {
+    console.log("create Room " + roomID);
     socket.join(roomID);
-    socket.emit('roomCreated', {roomID, color:'white'});
-    
+    socket.emit("roomCreated", { roomID, color: "white" });
   });
 
-  socket.on('joinRoom',(roomID) => {
-    
+  socket.on("joinRoom", (roomID) => {
     const room = io.sockets.adapter.rooms.get(roomID);
-    
-    if (room && room.size === 1){
-      console.log('ROOM JOINED');
+
+    if (room && room.size === 1) {
+      console.log("ROOM JOINED");
       socket.join(roomID);
-      socket.emit('roomJoined', {roomID, color: 'black'});
-      io.in(roomID).emit('startGame', {roomID});
-    }else{
-      
-      socket.emit('error','Room does not exist or is already full.');
+      socket.emit("roomJoined", { roomID, color: "black" });
+      io.in(roomID).emit("startGame", { roomID });
+    } else {
+      socket.emit("error", "Room does not exist or is already full.");
     }
   });
 
-  socket.on('makeMove',({roomNumber,move})=>{
-    
-    socket.to(roomNumber).emit('makeMove',move);
-  })
+  socket.on("makeMove", ({ roomNumber, move }) => {
+    socket.to(roomNumber).emit("makeMove", move);
+  });
 });
 
-app.get('/',function(req,res){
-  res.send('m715q');
-})
+app.get("/", function (req, res) {
+  res.send("m715q");
+});
 
-app.get('/api/test',function(req,res){
-  res.json({message:'Hey'});
-})
+app.get("/api/test", function (req, res) {
+  res.json({ message: "Hey" });
+});
 
-server.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function () {
   console.log("app listening on port 3000");
 });
